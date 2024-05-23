5d697b385e8daef4cfbedd7343d4cc54fc06e2dc
wilysaurio699 <137814632+wilysaurio699@users.noreply.github.com>
2024-05-23 10:00:43 -0500

    Ultimos cambios

--- Ejercicios 2/hemo.js
+++ Ejercicios 2/hemo.js
@@ -1,1 +1,10 @@
-var genero = alert("ingrese el genero del paciente");
+var genero = prompt("Ingrese el género del paciente para continuar");
+
+if(genero === "1") {
+    alert("El género del paciente ingresado es Masculino");
+} else if(genero === "2"){
+    alert("El género del paciente es Femenino");
+} else {
+    alert("Opción inválida. Por favor, ingrese 1 para Masculino o 2 para Femenino.");
+}
+
